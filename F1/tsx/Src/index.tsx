9371de82c95cefcc7faae5c20d0ec0fb3e78887f
dimdomin <145801131+dimdomin@users.noreply.github.com>
2024-07-04 08:23:55 +0300

    Delete F1 directory

--- F1/tsx/Src/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import { render } from "react-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import App from "./component/App";
-
-render(<App/>, document.getElementById("root"));
